5dad658c9874bb3a51ea275cfe2a2a92100ed1fe
natanros <natanielros@gmail.com>
2025-10-22 08:58:56 -0300

    fix: corrigir configuração postcss e tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   darkMode: "class", // Ativa modo dark via classe 'dark'
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
+export default config;
